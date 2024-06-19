4be874a74a0fb478e8472f1c5775e74140443a34
sxmilkii <163226503+sxmilkii@users.noreply.github.com>
2024-06-19 23:23:38 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kretinin_pr23106_pz10"
+include(":app")
+ 
---
+++ src/main/java/com/example/pr10/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.pr10;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+    };
+}
+
+
+
